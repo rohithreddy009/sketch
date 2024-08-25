e268a40a6ebe7c7e10eda3369077daeb346bf8b3
Rohith Reddy <89250524+rohithreddy009@users.noreply.github.com>
2024-08-25 13:32:59 +0530

    UI changes

--- app/(routes)/workspace/[fileId]/page.tsx
+++ app/(routes)/workspace/[fileId]/page.tsx
@@ -1,32 +1,33 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import WorkspaceHeader from '../_components/WorkspaceHeader'
-import Editor from '../_components/Editor'
+
+import React, { useEffect, useState } from 'react';
+import WorkspaceHeader from '../_components/WorkspaceHeader';
+import Editor from '../_components/Editor';
+import Canvas from '../_components/Canvas';
 import { useConvex } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { FILE } from '../../dashboard/_components/FileList';
-import Canvas from '../_components/Canvas';
 
-function Workspace({params}:any) {
-  const [triggerSave,setTriggerSave]=useState(false);
-  const [focusedSide, setFocusedSide] = useState('both');
-  const convex=useConvex();
-  const [fileData,setFileData]=useState<FILE|any>();
+function Workspace({ params }: any) {
+  const [triggerSave, setTriggerSave] = useState(false);
+  const [focusedSide, setFocusedSide] = useState<'document' | 'both' | 'canvas'>('both');
+  const convex = useConvex();
+  const [fileData, setFileData] = useState<FILE | any>();
 
-  useEffect(()=>{
-    console.log("FILEID",params.fileId)
-    params.fileId&&getFileData();
-  },[])
+  useEffect(() => {
+    console.log("FILEID", params.fileId);
+    params.fileId && getFileData();
+  }, [params.fileId]);
 
-  const getFileData=async()=>{
-    const result=await convex.query(api.files.getFileById,{_id:params.fileId})
+  const getFileData = async () => {
+    const result = await convex.query(api.files.getFileById, { _id: params.fileId });
     setFileData(result);
-  }
+  };
 
   return (
     <div className="flex flex-col h-screen">
       <WorkspaceHeader 
-        onSave={()=>setTriggerSave(!triggerSave)}
+        onSave={() => setTriggerSave(!triggerSave)}
         focusedSide={focusedSide}
         setFocusedSide={setFocusedSide}
       />
@@ -40,6 +41,9 @@ function Workspace({params}:any) {
             />
           </div>
         )}
+        {focusedSide === 'both' && (
+          <div className="w-0.5 px-0.5 bg-gray-900 dark:bg-gray-900" />
+        )}
         {(focusedSide === 'both' || focusedSide === 'canvas') && (
           <div className={`${focusedSide === 'both' ? 'w-1/2' : 'w-full'} h-full overflow-hidden`}>
             <Canvas
@@ -51,7 +55,7 @@ function Workspace({params}:any) {
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Workspace
+export default Workspace;
--- app/(routes)/workspace/_components/Canvas.tsx
+++ app/(routes)/workspace/_components/Canvas.tsx
@@ -1,44 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { Excalidraw } from "@excalidraw/excalidraw";
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { Excalidraw } from '@excalidraw/excalidraw';
 import { FILE } from '../../dashboard/_components/FileList';
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
-function Canvas({onSaveTrigger, fileId, fileData}: {onSaveTrigger: any, fileId: any, fileData: FILE}) {
+function Canvas({ onSaveTrigger, fileId, fileData }: { onSaveTrigger: any, fileId: any, fileData: FILE }) {
   const [whiteBoardData, setWhiteBoardData] = useState<any>();
-  const updateWhiteboard = useMutation(api.files.updateWhiteboard)
+  const updateWhiteboard = useMutation(api.files.updateWhiteboard);
 
   useEffect(() => {
-    onSaveTrigger && saveWhiteboard();
-  }, [onSaveTrigger])
+    if (onSaveTrigger) {
+      saveWhiteboard();
+    }
+  }, [onSaveTrigger]);
 
   const saveWhiteboard = () => {
     updateWhiteboard({
       _id: fileId,
-      whiteboard: JSON.stringify(whiteBoardData)
-    }).then(resp => console.log(resp))
-  }
+      whiteboard: JSON.stringify(whiteBoardData),
+    }).then(resp => console.log(resp));
+  };
 
   return (
     <div className="h-full w-full">
-      {fileData && <Excalidraw
-        theme='light'
-        initialData={{
-          elements: fileData?.whiteboard && JSON.parse(fileData?.whiteboard)
-        }}
-        onChange={(excalidrawElements, appState, files) =>
-          setWhiteBoardData(excalidrawElements)}
-        UIOptions={{
-          canvasActions: {
-            saveToActiveFile: false,
-            loadScene: false,
-            export: false,
-            toggleTheme: false
-          }
-        }}
-      />}
+      {fileData && (
+        <Excalidraw
+          theme='light'
+          initialData={{
+            elements: fileData?.whiteboard ? JSON.parse(fileData?.whiteboard) : [],
+          }}
+          onChange={(excalidrawElements) => setWhiteBoardData(excalidrawElements)}
+          UIOptions={{
+            canvasActions: {
+              saveToActiveFile: false,
+              loadScene: false,
+              export: false,
+              toggleTheme: false,
+            },
+          }}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default Canvas
+export default Canvas;
